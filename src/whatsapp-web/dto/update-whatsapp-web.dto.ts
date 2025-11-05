053a98b5d9424a7506503038ca8da3f9648622eb
KDEV999 <cristhiangrojas@gmail.com>
2025-11-05 16:31:56 -0500

    remove whatsapp web module

--- src/whatsapp-web/dto/update-whatsapp-web.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateWhatsappWebDto } from './create-whatsapp-web.dto';
-
-export class UpdateWhatsappWebDto extends PartialType(CreateWhatsappWebDto) {}
